2a6df59aa84d1be5cfe1270c0aa24decdd7c2ea7
TA2k <tombox2020@gmail.com>
2023-02-17 15:13:15 +0100

    fix linting

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-const axios = require("axios").default;
 const { request } = require("urllib");
 const Json2iob = require("json2iob");
 const Cam = require("onvif").Cam;
@@ -30,7 +29,6 @@ class Onvif extends utils.Adapter {
     this.deviceArray = [];
     this.devices = {};
     this.json2iob = new Json2iob(this);
-    this.requestClient = axios.create();
   }
 
   /**
@@ -62,7 +60,9 @@ class Onvif extends utils.Adapter {
         })
         .catch(async (err) => {
           this.log.error(`Error initializing device: ${err} device: ${JSON.stringify(device.native)}`);
-          this.log.error(`You can change user and password under object and edit device or delete device under objects and restart adapter`);
+          this.log.error(
+            `You can change user and password under object and edit device or delete device under objects and restart adapter`,
+          );
           this.log.error(err.stack);
           return null;
         });
@@ -94,11 +94,12 @@ class Onvif extends utils.Adapter {
     }
     await this.setObjectNotExistsAsync(device.native.id + ".events." + id, {
       type: "state",
-
       common: {
         name: name,
         type: "mixed",
         role: "indicator",
+        read: true,
+        write: false,
       },
       native: {},
     });
@@ -122,7 +123,9 @@ class Onvif extends utils.Adapter {
         if (err) {
           return;
         }
-        const urn = result["Envelope"]["Body"][0]["ProbeMatches"][0]["ProbeMatch"][0]["EndpointReference"][0]["Address"][0].payload;
+        const urn =
+          result["Envelope"]["Body"][0]["ProbeMatches"][0]["ProbeMatch"][0]["EndpointReference"][0]["Address"][0]
+            .payload;
         const xaddrs = result["Envelope"]["Body"][0]["ProbeMatches"][0]["ProbeMatch"][0]["XAddrs"][0].payload;
         let scopes = result["Envelope"]["Body"][0]["ProbeMatches"][0]["ProbeMatch"][0]["Scopes"][0].payload;
         scopes = scopes.split(" ");
@@ -159,24 +162,39 @@ class Onvif extends utils.Adapter {
           this.log.error("Error parsing scopes: " + error);
           this.log.error(error.stack);
         }
-        this.log.info(`Discovery Reply from ${rinfo.address} (${scopeObject.name}) (${scopeObject.hardware}) (${xaddrs}) (${urn})`);
+        this.log.info(
+          `Discovery Reply from ${rinfo.address} (${scopeObject.name}) (${scopeObject.hardware}) (${xaddrs}) (${urn})`,
+        );
         if (this.devices[rinfo.address]) {
           this.log.info(
-            `Skip device ${rinfo.address} because it is already configured via iobroker object. Delete the device under objects for reconfigure.`
+            `Skip device ${rinfo.address} because it is already configured via iobroker object. Delete the device under objects for reconfigure.`,
           );
           return;
         }
 
-        this.log.info(`Try to login to ${rinfo.address}:${cam.port}` + " with " + this.config.username + ":" + this.config.password);
-        await this.initDevice({ ip: rinfo.address, port: cam.port, username: this.config.username, password: this.config.password })
+        this.log.info(
+          `Try to login to ${rinfo.address}:${cam.port}` + " with " + this.config.username + ":" + this.config.password,
+        );
+        await this.initDevice({
+          ip: rinfo.address,
+          port: cam.port,
+          username: this.config.username,
+          password: this.config.password,
+        })
           .then(async (cam) => {
             this.log.info("Device successful initialized: " + cam.hostname + ":" + cam.port);
-            await this.fetchCameraInfos(cam, rinfo);
+            const native = await this.fetchCameraInfos(cam, rinfo);
             this.devices[cam.hostname] = cam;
             cam.on("event", this.processEvent.bind(this, { native: native }));
           })
           .catch((err) => {
-            this.log.error(`Failed to login to ${rinfo.address}:${cam.port}` + " with " + this.config.username + ":" + this.config.password);
+            this.log.error(
+              `Failed to login to ${rinfo.address}:${cam.port}` +
+                " with " +
+                this.config.username +
+                ":" +
+                this.config.password,
+            );
             this.log.error("Erro " + err);
             this.log.error(err.stack);
           });
@@ -357,13 +375,29 @@ class Onvif extends utils.Adapter {
       });
     });
 
-    this.json2iob.parse(id + ".general", cam, { forceIndex: true, removePasswords: true, channelName: "General Information" });
+    this.json2iob.parse(id + ".general", cam, {
+      forceIndex: true,
+      removePasswords: true,
+      channelName: "General Information",
+    });
     this.json2iob.parse(id + ".infos.timeDate", timeDate, { forceIndex: true, channelName: "Time Date" });
     this.json2iob.parse(id + ".infos.presets", presets, { forceIndex: true, channelName: "Presets of active source" });
-    this.json2iob.parse(id + ".infos.deviceInformation", deviceInformation, { forceIndex: true, channelName: "Device Information" });
-    this.json2iob.parse(id + ".infos.deviceProfiles", deviceProfiles, { forceIndex: true, channelName: "Device Profiles" });
-    this.json2iob.parse(id + ".infos.deviceCapabilities", deviceCapabilities, { forceIndex: true, channelName: "Device Capabilities" });
-    this.json2iob.parse(id + ".infos.deviceServices", deviceServices, { forceIndex: true, channelName: "Device Services" });
+    this.json2iob.parse(id + ".infos.deviceInformation", deviceInformation, {
+      forceIndex: true,
+      channelName: "Device Information",
+    });
+    this.json2iob.parse(id + ".infos.deviceProfiles", deviceProfiles, {
+      forceIndex: true,
+      channelName: "Device Profiles",
+    });
+    this.json2iob.parse(id + ".infos.deviceCapabilities", deviceCapabilities, {
+      forceIndex: true,
+      channelName: "Device Capabilities",
+    });
+    this.json2iob.parse(id + ".infos.deviceServices", deviceServices, {
+      forceIndex: true,
+      channelName: "Device Services",
+    });
     this.json2iob.parse(id + ".infos.deviceServicesCapabilities", deviceServicesCapabilities, {
       forceIndex: true,
       channelName: "Device Services Capabilities",
@@ -372,6 +406,7 @@ class Onvif extends utils.Adapter {
     this.json2iob.parse(id + ".infos.videoSources", videoSources, { forceIndex: true, channelName: "Video Sources" });
     this.json2iob.parse(id + ".infos.status", status, { forceIndex: true, channelName: "Status" });
     this.json2iob.parse(id + ".infos.streamUris", streamUris, { forceIndex: true, channelName: "Stream Uris" });
+    return native;
   }
 
   async initDevice(device) {
@@ -388,8 +423,9 @@ class Onvif extends utils.Adapter {
           if (err) {
             reject(err);
           }
+          // @ts-ignore
           resolve(this);
-        }
+        },
       );
     });
   }
@@ -408,7 +444,6 @@ class Onvif extends utils.Adapter {
         if (response.status === 401) {
           return await request(snapshotUrl, {
             method: "GET",
-            rejectUnauthorized: false,
             digestAuth: `${deviceObject.native.user}:${deviceObject.native.password}`,
           })
             .then((response) => {
@@ -435,24 +470,24 @@ class Onvif extends utils.Adapter {
   }
 
   generateRange(startIp, endIp) {
-    var startLong = toLong(startIp);
-    var endLong = toLong(endIp);
+    let startLong = this.toLong(startIp);
+    let endLong = this.toLong(endIp);
     if (startLong > endLong) {
-      var tmp = startLong;
+      const tmp = startLong;
       startLong = endLong;
       endLong = tmp;
     }
-    var rangeArray = [];
-    var i;
+    const rangeArray = [];
+    let i;
     for (i = startLong; i <= endLong; i++) {
-      rangeArray.push(fromLong(i));
+      rangeArray.push(this.fromLong(i));
     }
     return rangeArray;
   }
 
   //toLong taken from NPM package 'ip'
   toLong(ip) {
-    var ipl = 0;
+    let ipl = 0;
     ip.split(".").forEach(function (octet) {
       ipl <<= 8;
       ipl += parseInt(octet);
@@ -491,8 +526,12 @@ class Onvif extends utils.Adapter {
     if (state) {
       if (!state.ack) {
         const deviceId = id.split(".")[2];
-        let command = id.split(".")[4];
+        const command = id.split(".")[4];
         const deviceObject = await this.getObjectAsync(deviceId);
+        if (!deviceObject || !deviceObject.native || !deviceObject.native.ip) {
+          this.log.warn("No ip found for " + deviceId);
+          return;
+        }
         const cam = this.devices[deviceObject.native.ip];
         if (command === "Refresh") {
           this.fetchCameraInfos(cam, { address: deviceObject.native.ip });
