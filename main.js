df43477b77655c8193ed368a73b5cfaffdc07446
TA2k <tombox2020@gmail.com>
2023-03-28 15:42:57 +0200

    add catches

--- main.js
+++ main.js
@@ -64,7 +64,9 @@ class Onvif extends utils.Adapter {
         .catch(async (error) => {
           this.log.error(`Data: ${error.data} XML: ${error.xml}`);
           this.log.error(`Error initializing device: ${error.err} device: ${JSON.stringify(device.native)}`);
-          this.log.error(`You can change user and password under object and edit device or delete device under objects and restart adapter`);
+          this.log.error(
+            `You can change user and password under object and edit device or delete device under objects and restart adapter`,
+          );
           this.log.error(error.err.stack);
           return null;
         });
@@ -160,7 +162,7 @@ class Onvif extends utils.Adapter {
   }
   async processEvent(device, event) {
     this.log.debug(`Received event: ${JSON.stringify(event)}`);
-    if (!event.topic) {
+    if (!event.topic || !event.topic._) {
       this.log.warn("Event without topic: " + JSON.stringify(event));
       this.sendSentry(event);
       return;
@@ -315,12 +317,14 @@ class Onvif extends utils.Adapter {
         this.log.info(`Discovery Reply from ${rinfo.address} (${scopeObject.name}) (${scopeObject.hardware}) (${xaddrs}) (${urn})`);
         if (this.devices[rinfo.address]) {
           this.log.info(
-            `Skip device ${rinfo.address} because it is already configured via iobroker object. Delete the device under objects for reconfigure.`
+            `Skip device ${rinfo.address} because it is already configured via iobroker object. Delete the device under objects for reconfigure.`,
           );
           return;
         }
 
-        this.log.info(`Try to login to ${rinfo.address}:${cam.port}` + " with " + this.config.user + ":" + this.maskPassword(this.config.password));
+        this.log.info(
+          `Try to login to ${rinfo.address}:${cam.port}` + " with " + this.config.user + ":" + this.maskPassword(this.config.password),
+        );
         await this.initDevice({
           ip: rinfo.address,
           port: cam.port,
@@ -337,7 +341,11 @@ class Onvif extends utils.Adapter {
           })
           .catch((error) => {
             this.log.error(
-              `Failed to login to ${rinfo.address}:${cam.port}` + " with " + this.config.user + ":" + this.maskPassword(this.config.password)
+              `Failed to login to ${rinfo.address}:${cam.port}` +
+                " with " +
+                this.config.user +
+                ":" +
+                this.maskPassword(this.config.password),
             );
             this.log.error("Error " + error.err);
             this.log.debug(error.err.stack);
@@ -427,16 +435,23 @@ class Onvif extends utils.Adapter {
         streamUris[profile.name].snapshotUrl = await promisify(cam.getSnapshotUri)
           .bind(cam)({ ProfileToken: profile.$.token })
           .catch((e) => {
-            this.log.warn(`${cam.hostname}:${cam.port} ${profile.name} No snapshot url available. Try to get it from the stream url via ffmpeg`);
+            this.log.warn(
+              `${cam.hostname}:${cam.port} ${profile.name} No snapshot url available. Try to get it from the stream url via ffmpeg`,
+            );
             this.log.warn(e);
           });
         if (!snapshotUrl && streamUris[profile.name].snapshotUrl) {
           snapshotUrl = streamUris[profile.name].snapshotUrl.uri;
           if (this.config.overwriteSnapshotPort) {
             //find generic port in url and replace it
-            const urlObject = new URL(snapshotUrl);
-            urlObject.port = cam.port;
-            snapshotUrl = urlObject.toString();
+            try {
+              const urlObject = new URL(snapshotUrl);
+              urlObject.port = cam.port;
+              snapshotUrl = urlObject.toString();
+            } catch (error) {
+              this.log.error("Failed to parse snapshot url" + snapshotUrl);
+              this.log.error(error);
+            }
           }
         }
         streamUris[profile.name].live_stream_tcp = await promisify(cam.getStreamUri)
@@ -641,7 +656,7 @@ class Onvif extends utils.Adapter {
           }
           // @ts-ignore
           resolve(this);
-        }
+        },
       );
       // @ts-ignore
       cam.on("rawResponse", (data) => {
@@ -886,9 +901,13 @@ class Onvif extends utils.Adapter {
             obj.from,
             obj.command,
             {
-              result: `Added ${this.discoveredDevices.length} cameras: ${JSON.stringify(this.discoveredDevices, null, 2)}. See log for details`,
+              result: `Added ${this.discoveredDevices.length} cameras: ${JSON.stringify(
+                this.discoveredDevices,
+                null,
+                2,
+              )}. See log for details`,
             },
-            obj.callback
+            obj.callback,
           );
       }
       if (obj.command === "manualSearch") {
@@ -902,7 +921,7 @@ class Onvif extends utils.Adapter {
             obj.from,
             obj.command,
             { result: `Found ${deviceArray.length} cameras: ${JSON.stringify(deviceArray, null, 2)}` },
-            obj.callback
+            obj.callback,
           );
       }
       if (obj.command === "snapshot") {
