a6f03730795d426bd66867715c4952950a2edd1c
TA2k <tombox2020@gmail.com>
2023-05-08 22:58:45 +0200

    hide missing status

--- main.js
+++ main.js
@@ -416,8 +416,8 @@ class Onvif extends utils.Adapter {
     const status = await promisify(cam.getStatus)
       .bind(cam)()
       .catch((e, data, xml) => {
-        this.log.error(`Data: ${JSON.stringify(data)} xml: ${xml}`);
-        this.log.error(`No status found for ${cam.hostname}:${cam.port} ${e}`);
+        this.log.debug(`Data: ${JSON.stringify(data)} xml: ${xml}`);
+        this.log.debug(`No status found for ${cam.hostname}:${cam.port} ${e}`);
       });
     const presets = await promisify(cam.getPresets)
       .bind(cam)()
